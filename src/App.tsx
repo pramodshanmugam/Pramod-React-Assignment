ca3e8f17d3f19afcfadf360d203ef63395087403
kodehash <kousick.shanmugam@gmail.com>
2022-12-20 16:05:32 +0530

    to do list

--- src/App.tsx
+++ src/App.tsx
@@ -20,9 +20,45 @@ const App: React.FC =() =>{
 
   };
 
-  const onDragEnd=(result:DropResult) =>{
-    console.log(result);
-  }
+  const onDragEnd = (result: DropResult) => {
+    const { destination, source } = result;
+
+    // console.log(result);
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    let add;
+    let active = todos;
+    let complete = completedTodos;
+    // Source Logic
+    if (source.droppableId === "TodosList") {
+      add = active[source.index];
+      active.splice(source.index, 1);
+    } else {
+      add = complete[source.index];
+      complete.splice(source.index, 1);
+    }
+
+    // Destination Logic
+    if (destination.droppableId === "TodosList") {
+      active.splice(destination.index, 0, add);
+    } else {
+      complete.splice(destination.index, 0, add);
+    }
+
+    setcompletedTodos(complete);
+    setTodos(active);
+
+  };
   return(
     <>
      <Navbar />
