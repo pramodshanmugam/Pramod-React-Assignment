2fa49f2198917c64fe4ebe9786934cbea6979db0
kodehash <kousick.shanmugam@gmail.com>
2022-12-21 23:41:09 +0530

    create issues page built

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,73 +1,100 @@
 import React from 'react';
+import { useForm } from 'react-hook-form'
 import './style.css';
 
+type create={
+    Title:string;
+    Type:string;
+    Status:string;
+    Priority:string;
+    Resolution:string;
+    AffectedVersion:string;
+    FixVersion:string;
+    Components:string;
+    Assignee:string;
+    Labels:string;
+    Reporter:string;
+    Sprint:string;
+    Description:string;
+    Storypoint:string;
+    Attachment:string;
+
+};
+
 function Create(){
+    const {register,handleSubmit} = useForm<create>()
+
+    const onSubmit = handleSubmit((data) => {
+        alert(JSON.stringify(data))
+    })
     return(
-        <form>
+        <form className='createform'onSubmit={onSubmit}>
             <div>
-                <label htmlFor="Title:">First Name</label>
-                <input className='Title'id="Title" name="Title" type="text"/>
+                <label htmlFor="Title:">Title</label>
+                <input className="textbox" id="textbox" name="textbox" type="text"/>
             </div>
             <div>
                 <label htmlFor="Type">Type </label>
-                <input className='Type'id="Tyoe" name="Type" type="text"/>
+                <input className='textbox'id="Type" name="Type" type="text"/>
             </div>
 
             <div>
                 <label htmlFor="Status">Status</label>
-                <input className='Status'id="Status" name="Status" type="text"/>
+                <input className='textbox'id="Status" name="Status" type="text"/>
             </div>
             <div>
                 <label htmlFor="Priority">Priority</label>
-                <input className='Priority'id="Priority" name="Priority" type="text"/>
+                <input className='textbox'id="Priority" name="Priority" type="text"/>
             </div>
             <div>
                 <label htmlFor="Resolution">Resolution</label>
-                <input className='Resolution'id="Resolution" name="Resolution" type="text"/>
+                <input className='textbox'id="Resolution" name="Resolution" type="text"/>
             </div>
             <div>
-                <label htmlFor="Affected-Version">Affected-Version</label>
-                <input className='Affected-Version'id="Affected-Version" name="Affected-Version" type="text"/>
+                <label htmlFor="AffectedVersion">Affected-Version</label>
+                <input className='textbox'id="AffectedVersion" name="AffectedVersion" type="text"/>
             </div>
             <div>
-                <label htmlFor="Fix-Version">Fix-Version</label>
-                <input className='Fix-Version'id="Fix-Version" name="Fix-Version" type="text"/>
+                <label htmlFor="FixVersion">Fix-Version</label>
+                <input className='textbox'id="FixVersion" name="FixVersion" type="text"/>
             </div>
 
             <div>
-                <label htmlFor="Components/s:">Components/s</label>
-                <input className='Components/s'id="Components/s" name="Components/s" type="text"/>
+                <label htmlFor="Components">Components/s</label>
+                <input className='textbox'id="Components" name="Components" type="text"/>
             </div>
             <div>
-                <label htmlFor="Assignee:">Assignee</label>
-                <input className='Assignee'id="Assignee" name="Assignee" type="text"/>
+                <label htmlFor="Assignee">Assignee</label>
+                <input className='textbox'id="Assignee" name="Assignee" type="text"/>
             </div>
             <div>
-                <label htmlFor="Labels:">Labels</label>
-                <input className='Labels'id="Labels" name="Labels" type="text"/>
+                <label htmlFor="Labels">Labels</label>
+                <input className='textbox'id="Labels" name="Labels" type="text"/>
             </div>
             <div>
-                <label htmlFor="Reporter:">Reporter</label>
-                <input className='Reporter'id="Reporter" name="Reporter" type="text"/>
+                <label htmlFor="Reporter">Reporter</label>
+                <input className='textbox'id="Reporter" name="Reporter" type="text"/>
             </div>
             <div>
-                <label htmlFor="Sprint:">Sprint</label>
-                <input className='Sprint'id="Sprint" name="Sprint" type="text"/>
+                <label htmlFor="Sprint">Sprint</label>
+                <input className='textbox'id="Sprint" name="Sprint" type="text"/>
             </div>
             <div>
-                <label htmlFor="Description:">Description</label>
-                <input className='Description'id="Description" name="Description" type="text"/>
+                <label htmlFor="Description">Description</label>
+                <input className='textbox'id="Description" name="Description" type="text"/>
             </div>
             
             <div>
-                <label htmlFor="Storypoint:">Storypoint</label>
-                <input className='Storypoint'id="Storypoint" name="Storypoint" type="text"/>
+                <label htmlFor="Storypoint">Storypoint</label>
+                <input className='textbox'id="Storypoint" name="Storypoint" type="text"/>
             </div>
             <div>
-                <label htmlFor="Attachment:">Attachment</label>
-                <input className='Attachment'id="Attachment" name="Attachment" type="text"/>
+                <label htmlFor="Attachment">Attachment</label>
+                <input className='textbox'id="Attachment" name="Attachment" type="text"/>
             </div>
+            
             <button className='submitbutton' type='submit'>Submit</button>
+            
 
 
         </form>
@@ -81,3 +108,6 @@ function Create(){
 }
 
 export default Create;
+
+
+
--- src/components/Issue.tsx
+++ src/components/Issue.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import  "./style.css";
+import "./style.css";
 // import '/react/assign1/src/App.css';
 import InputField from './InputField';
 import { Todo } from './model';
